8d5984c05db4a096582b26dcea0e0e7ca7b4933d
Varun Kashyap <kvarun10@gmail.com>
2018-05-24 02:04:33 +0530

    First successful setup

---
+++ business-logic/aes1.js
@@ -0,0 +1,1 @@
+var sdk=require('fabric-client');
--- main.js
+++ main.js
@@ -1,14 +1,49 @@
-const express = require('express')
-const app = express()
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.post('/api/login', function (req, res) {
-  // ...
-})
+var indexRouter = require('./routes/index');
+require('nw');
+//require('nw.gui').Window.get().showDevTools();
 
-app.post('/api/ping', function (req, res) {
-  // ...
-})
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/'));
+app.set('views', __dirname + '/src');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-app.post('/api/logout', function (req, res) {
-  // ...
-})
+// view engine setup
+//app.set('views', path.join(__dirname, '/src'));
+//app.set(' engine', 'html');
+//app.engine('html', require('ejs').renderFile);
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.get('/',function(req, res){
+    res.render('index.html');
+} )
+
+app.listen(app.get('port'),function(){
+
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+
+
+//module.exports = app;
+
+
